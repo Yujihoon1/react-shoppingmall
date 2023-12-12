dcd2b6cc4c10b5ef9e6f4382ded53d03fa0abd5d
Yujihoon1 <yjh150062@naver.com>
2023-12-13 04:00:41 +0900

    로그인 기능 구현
    1. useState를 사용하여 Id,Password 다루기
    2.async+await를 통해 비동기적 통신

--- src/js/Login/Login.js
+++ src/js/Login/Login.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 //로그인 페이지
 function Login() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user_id: id, password }),
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      //로그인 성공 시
+      console.log("로그인 성공");
+    } else {
+      //로그인 실패 시
+      console.log("로그인 실패");
+    }
+  };
+
   return (
     <div>
       <h2>로그인</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           아이디:
-          <input type="text" name="id" />
+          <input
+            type="text"
+            name="id"
+            onChange={(e) => setId(e.target.value)}
+          />
         </label>
         <br />
         <label>
           비밀번호:
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
         <br />
         <input type="submit" value="로그인" />
