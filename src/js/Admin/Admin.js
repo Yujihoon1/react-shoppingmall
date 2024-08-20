f3292e54563a8ad5a1e8ed529bfdd1991f7b00c5
Yujihoon1 <yjh150062@naver.com>
2024-08-20 17:11:44 +0900

    Admin
    Admin - 관리자 페이지
    1. 관리자 nav 바
     1-1) 대시보드
     1-2) 주문 관리
     1-3) 재고 관리
     1-4) 리뷰 관리
     1-5) 프로모션 관리
     1-6) 통계 및 보고서

--- src/js/Admin/Admin.js
+++ src/js/Admin/Admin.js
@@ -1,8 +1,66 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Dashboard from "./Dashboard";
+import OrderManagement from "./OrderManagement";
+import InventoryManagement from "./InventoryManagement";
+import ProductAdd from "../Product/ProductAdd";
+import ProductUpdate from "../Product/ProductUpdate";
+// import ReviewManagement from "./ReviewManagement";
+import PromotionManagement from "./PromotionManagement";
+// import Statistics from "./Statistics";
+import PromotionAdd from "../Promotion/PromotionAdd";
+import PromotionEdit from "../Promotion/PromotionEdit";
 
 //관리자 페이지
 function Admin() {
-  return <></>;
+  return (
+    <div className="admin-dashboard">
+      <nav className="admin-navigation">
+        <ul>
+          <li>
+            <Link to="/admin/dashboard">대시보드</Link>
+          </li>
+          <li>
+            <Link to="/admin/orders">주문 관리</Link>
+          </li>
+          <li>
+            <Link to="/admin/inventory">재고 관리</Link>
+          </li>
+          <li>
+            <Link to="/admin/reviews">리뷰 관리</Link>
+          </li>
+          <li>
+            <Link to="/admin/promotions">프로모션 관리</Link>
+          </li>
+          <li>
+            <Link to="/admin/statistics">통계 및 보고서</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="admin-content">
+        <Routes>
+          {/* <Route path="/admin/dashboard" element={<Dashboard />} /> */}
+          <Route path="orders" element={<OrderManagement />} />
+          <Route path="inventory" element={<InventoryManagement />} />
+          <Route path="products/add" element={<ProductAdd />} />
+          <Route
+            path="products/update/:product_num"
+            element={<ProductUpdate />}
+          />
+          {/* <Route path="/admin/reviews" element={<ReviewManagement />} /> */}
+          <Route path="promotions" element={<PromotionManagement />} />
+          <Route path="promotions/add" element={<PromotionAdd />} />
+          <Route
+            path="promotions/edit/:promotionId"
+            element={<PromotionEdit />}
+          />
+          {/* <Route path="/admin/statistics" element={<Statistics />} /> */}
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default Admin;
