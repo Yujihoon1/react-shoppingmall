fd68c204b158647d18954d4afa5e891fb665e96b
Yujihoon1 <yjh150062@naver.com>
2024-08-20 17:09:59 +0900

    Main Content
    메인 화면의 MainContent 관리
    1. Promotion 창
     1-1) 현재 활성화된 프로모션 데이터 반환
    2. 상품 관련 창
     2-1) 상품 사진
     2-2) 상품 구매 수량
     2-3) 상품 구매
     2-4) 상품 찜

--- src/js/MainContent/MainContent.js
+++ src/js/MainContent/MainContent.js
@@ -2,9 +2,27 @@ import React, { useState, useEffect } from "react";
 import ProductCard from "../Product/ProductCard";
 import ProductForm from "../Product/ProductForm";
 import axios from "axios";
+import "./MainContent.css";
+import { Link } from "react-router-dom";
+
+//fetchPromotionBanner
+const fetchPromotionBanner = async () => {
+  const today = new Date().toISOString().split("T")[0]; // 오늘 날짜를 'YYYY-MM-DD' 형식으로 변환
+  try {
+    const response = await axios.get(
+      `http://localhost:5000/promotions/active?date=${today}`
+    );
+    return response.data; // 활성화된 프로모션 데이터 반환
+  } catch (error) {
+    console.error("Error fetching active promotions:", error);
+    return []; // 오류가 발생한 경우 빈 배열 반환
+  }
+};
 
 const MainContent = () => {
   const [product, setProduct] = useState([]);
+  const [activePromotions, setActivePromotions] = useState([]);
+  const [currentBanner, setCurrentBanner] = useState(0);
   const [quantity, setQuantity] = useState(1); // 구매 수량 관리
 
   useEffect(() => {
@@ -22,8 +40,26 @@ const MainContent = () => {
       }
     };
 
+    const getActivePromotions = async () => {
+      const promotions = await fetchPromotionBanner();
+      setActivePromotions(promotions);
+    };
+
+    getActivePromotions();
     fetchProduct();
   }, []);
+
+  const prevBanner = () => {
+    setCurrentBanner(
+      currentBanner === 0 ? activePromotions.length - 1 : currentBanner - 1
+    );
+  };
+
+  const nextBanner = () => {
+    setCurrentBanner(
+      currentBanner === activePromotions.length - 1 ? 0 : currentBanner + 1
+    );
+  };
 
   const handleProductDetail = (product) => {
     // 상품상세 정보보기
@@ -42,24 +78,47 @@ const MainContent = () => {
 
   return (
     <>
-      <div className="banner">배너</div>
-      <section id="product-list">
+      <div className="banner">
+        {activePromotions.length > 0 && (
+          <>
+            <div className="prev" onClick={prevBanner}>
+              Prev
+            </div>
+            <Link
+              to={`/promotion/${activePromotions[currentBanner].promotion_id}`}
+            >
+              <img
+                src={`http://localhost:5000/${activePromotions[currentBanner].promotion_banner_image_path}`}
+                alt={`Active Promotion ${currentBanner}`}
+              />
+            </Link>
+            <div className="next" onClick={nextBanner}>
+              Next
+            </div>
+          </>
+        )}
+      </div>
+      <div className="product-list">
         {product.map((productItem) => (
-          <React.Fragment key={productItem.num}>
-            <ProductCard
-              product={productItem}
-              onProductDetail={handleProductDetail}
-            />
-            <ProductForm
-              product={productItem}
-              onPurchase={handlePurchase}
-              onAddToCart={handleAddToCart}
-              quantity={quantity}
-              onQuantityChange={setQuantity}
-            />
-          </React.Fragment>
+          <div key={productItem.num} className="product-card">
+            <React.Fragment key={productItem.num}>
+              <ProductCard
+                product={productItem}
+                onProductDetail={handleProductDetail}
+              />
+              <div className="product-form">
+                <ProductForm
+                  product={productItem}
+                  onPurchase={handlePurchase}
+                  onAddToCart={handleAddToCart}
+                  quantity={quantity}
+                  onQuantityChange={setQuantity}
+                />
+              </div>
+            </React.Fragment>
+          </div>
         ))}
-      </section>
+      </div>
     </>
   );
 };
