88622bdc321262d27812b1562e5929aebe9a1baf
Yujihoon1 <yjh150062@naver.com>
2024-09-08 18:18:15 +0900

    MyPage
    사용자 페이지

--- src/js/MyPage/MyPage.js
+++ src/js/MyPage/MyPage.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import AuthContext from "../../contexts/AuthContext";
+import axios from "axios";
 
 function MyPage() {
-  return <></>;
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`http://localhost:5000/mypage/${user.user_num}`)
+        .then((response) => {
+          console.log(response.data); // 응답 데이터를 콘솔에 출력
+        })
+        .catch((error) => {
+          console.error("Error:", error); // 에러 발생 시 에러 메시지를 콘솔에 출력
+        });
+    }
+  }, [user]);
+
+  return (
+    <div>
+      <h2>마이 페이지</h2>
+      <p>사용자 이름: {user && user.user_name}</p>
+    </div>
+  );
 }
 
 export default MyPage;
