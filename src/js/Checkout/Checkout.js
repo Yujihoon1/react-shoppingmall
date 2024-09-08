a2d27aab16ab8992e94cb8a001b7e1b7886c1210
Yujihoon1 <yjh150062@naver.com>
2024-09-08 18:18:03 +0900

    Checkout
    상품 계산
    1. 각 아이템별 총 가격을 계산해줌
     - 여러 아이템들을 선택해서 계산할때

--- src/js/Checkout/Checkout.js
+++ src/js/Checkout/Checkout.js
@@ -7,8 +7,13 @@ import HandlePayment from "../Checkout/HandlePayment";
 
 function Checkout() {
   const location = useLocation();
-  const { product, quantity } = location.state;
-  const totalPrice = product.product_price * quantity; // 총 가격 계산
+  const { items } = location.state;
+
+  // 총 가격 계산
+  const totalPrice = items.reduce(
+    (acc, item) => acc + item.product.product_price * item.quantity,
+    0
+  );
 
   const [isAgreed, setIsAgreed] = useState(false);
 
@@ -26,16 +31,35 @@ function Checkout() {
 
   return (
     <>
-      <article id={`product-card-${product.product_num}`}>
-        <div className="product-img">
-          <img alt={`${product.product_name} Image`} />
-        </div>
-        <h3 className="product-name">상품 이름: {product.product_name}</h3>
-        <p className="product-price">가격: {product.product_price}원</p>
-        <p className="product-content">상품 : {product.product_content}</p>
-        <p className="quantity">상품 갯수:{quantity} 개</p>
-        <p className="total-price">총 가격: {totalPrice} 원</p>
-      </article>
+      {items.map((item, index) => {
+        // 각 아이템별 총 가격 계산
+        const itemTotalPrice = item.product.product_price * item.quantity;
+
+        return (
+          <article id={`product-card-${item.product.product_num}`} key={index}>
+            <div className="product-img">
+              <img
+                src={`http://localhost:5000/${item.product.product_image}`}
+                alt={`${item.product.product_name} Image`}
+              />
+            </div>
+            <h3 className="product-name">
+              상품 이름: {item.product.product_name}
+            </h3>
+            <p className="product-price">
+              가격: {item.product.product_price}원
+            </p>
+            <p className="product-content">
+              상품 설명: {item.product.product_content}
+            </p>
+            <p className="quantity">상품 갯수: {item.quantity} 개</p>
+            <p className="item-total-price">
+              해당 상품 총 가격: {itemTotalPrice} 원
+            </p>
+          </article>
+        );
+      })}
+      <p className="total-price">총 결제 금액: {totalPrice} 원</p>
       <MyInfo onMyInfoChange={setMyInfo} />
       <Payment onPaymentInfoChange={setPaymentInfo} />
       <div>
@@ -51,8 +75,8 @@ function Checkout() {
         isAgreed={isAgreed}
         myInfo={myInfo}
         paymentInfo={paymentInfo}
-        product={product}
-        quantity={quantity}
+        items={items}
+        totalPrice={totalPrice}
       />
     </>
   );
